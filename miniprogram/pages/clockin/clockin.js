0c5468cae4a1d881582f2ad985d2cc65607a31a0
ltfhxdz <ltfhxdz@gmail.com>
2021-04-22 22:54:08 +0800

    初步完成打卡显示

--- miniprogram/pages/clockin/clockin.js
+++ miniprogram/pages/clockin/clockin.js
@@ -1,18 +1,59 @@
-// miniprogram/pages/clockin/clockin.js
+// miniprogram/pages/setup/setup.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
   },
+
+  activityQuery: function () {
+    let bigList2 = [];
+    let smallList2 = [];
+    const db = wx.cloud.database();
+    db.collection('big').get({
+      success: res => {
+        bigList2 = res.data;
+
+        const db = wx.cloud.database();
+        db.collection('small').get({
+          success: res => {
+            smallList2 = res.data;
+
+            let twoList = [];
+            for (let x in bigList2) {
+              let twoMap = {};
+              twoMap["id"] = bigList2[x]["_id"];
+              twoMap["name"] = bigList2[x]["name"];
+              let twodata = [];
+              for (let y in smallList2) {
+                if (bigList2[x]["_id"] == smallList2[y]["big_id"]) {
+                  let twoDataMap = {};
+                  twoDataMap["name"] = smallList2[y]["name"];
+                  twodata.push(twoDataMap);
+                }
+              }
+
+              twoMap["twodata"] = twodata;
+              twoList.push(twoMap);
+
+            }
+            this.setData({
+              twoList: twoList
+            })
+          }
+        })
+      }
+    })
+  },
+
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.activityQuery();
   },
 
   /**
--- miniprogram/pages/setup/setup.js
+++ miniprogram/pages/setup/setup.js
@@ -6,13 +6,122 @@ Page({
    */
   data: {
     bigAddHidden: true,
+    smallAddHidden: true
   },
+
+  addActivity: function (e) {
+    this.smallQuery(e.currentTarget.dataset.id);
+
+    this.setData({
+      big_id: e.currentTarget.dataset.id,
+      activity: e.currentTarget.dataset.name,
+      activityFlag: true
+    })
+  },
+
+  smallDelete: function (e) {
+    const db = wx.cloud.database();
+    db.collection('small').doc(e.currentTarget.dataset.id).remove({
+      success: res => {
+        this.smallQuery(this.data.big_id);
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '数据库删除失败'
+        })
+        console.error('数据库删除失败：', err)
+      }
+    })
+
+  },
+
+  smallAddDB: function (big_id, name) {
+    const db = wx.cloud.database()
+    db.collection('small').add({
+      data: {
+        big_id: big_id,
+        name: name
+      },
+      success: res => {
+        this.smallQuery(this.data.big_id);
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '数据库新增失败'
+        })
+        console.error('数据库新增失败：', err)
+      }
+    })
+
+  },
+
+  smallAdd: function () {
+    this.smallAddClean();
+    this.setData({
+      smallAddHidden: false
+    })
+  },
+
+  smallAddCancel: function () {
+    this.setData({
+      smallAddValue: '',
+      smallAddHidden: true,
+      smallAddFlag: false
+    });
+  },
+
+
+  smallAddConfirm: function () {
+    this.smallAddDB(this.data.big_id, this.data.smallName);
+
+
+    this.setData({
+      smallAddHidden: true
+    });
+
+  },
+
+
+  smallAddInput: function (e) {
+    if (e.detail.value.length == 0) {
+      this.smallAddClean();
+    } else {
+      this.setData({
+        smallName: e.detail.value,
+        smallAddFlag: true
+      })
+    }
+  },
+
+  smallAddClean: function () {
+    this.setData({
+      smallAddValue: '',
+      smallAddFlag: false
+    })
+  },
+
+
+  smallQuery: function (big_id) {
+    const db = wx.cloud.database();
+    db.collection('small').where({
+      big_id: big_id
+    }).get({
+      success: res => {
+        this.setData({
+          smallList: res.data
+        })
+      }
+    })
+  },
+
 
   bigDelete: function (e) {
     const db = wx.cloud.database();
     db.collection('big').doc(e.currentTarget.dataset.id).remove({
       success: res => {
-        this.query();
+        this.bigQuery();
       },
       fail: err => {
         wx.showToast({
@@ -32,7 +141,7 @@ Page({
         name: name
       },
       success: res => {
-        this.query();
+        this.bigQuery();
       },
       fail: err => {
         wx.showToast({
@@ -91,8 +200,7 @@ Page({
   },
 
 
-  query: function () {
-    let resultList = [];
+  bigQuery: function () {
     const db = wx.cloud.database();
     db.collection('big').get({
       success: res => {
@@ -122,7 +230,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.query();
+    this.bigQuery();
   },
 
   /**
